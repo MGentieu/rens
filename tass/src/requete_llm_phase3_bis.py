46a67e28136d0dc644eb6a8f9b9e9bb8de1b9ad1
MGentieu <mgentieu02@gmail.com>
2025-11-07 13:49:35 +0000

    Test pour pull

--- tass/src/requete_llm_phase3_bis.py
+++ tass/src/requete_llm_phase3_bis.py
@@ -10,102 +10,43 @@ client = Mistral(api_key=api_key)
 
 prompt = """You are an assistant responsible for analyzing news articles.
 For each article, you must add a structured field containing a semantic analysis.
-Here are the expected fields:
-"""
+Here are the expected fields to add to each article :
 
-structured_output = '''
-"structured": {
-    "topics": [
-        "military exercises",
-        "anti-terrorism"
-    ],
-    "overall_sentiment": 0.0,
-    "sentiment_by_country": [
-        {
-            "country": "TJ",
-            "sentiment": 0.0
-        },
-        {
-            "country": "RU",
-            "sentiment": 0.0
-        },
-        {
-            "country": "AM",
-            "sentiment": 0.0
-        },
-        {
-            "country": "BY",
-            "sentiment": 0.0
-        },
-        {
-            "country": "KZ",
-            "sentiment": 0.0
-        },
-        {
-            "country": "KG",
-            "sentiment": 0.0
-        },
-        {
-            "country": "UZ",
-            "sentiment": 0.0
-        }
-    ],
-    "actors_mentioned": [
-        "Commonwealth of Independent States (CIS)",
-        "Collective Security Treaty Organization (CSTO)",
-        "Armenia",
-        "Belarus",
-        "Kazakhstan",
-        "Kyrgyzstan",
-        "Russia",
-        "Tajikistan",
-        "Uzbekistan"
-    ],
-    "article_type": "report",
-    "id": 2034673
+strctured : {
+    topics: the 2 main themes of the article, expressed in English (e.g., "military", "technology", "politics", "economy", "diplomacy", "environment", "health", "science", etc.).
+
+    overall_sentiment: a global sentiment score between -1 (very negative) and 1 (very positive), with neutral sentiment close to 0.
+
+    sentiment_by_country: a list of objects { "country": "<ISO alpha-2 code>", "sentiment": <float> } corresponding to the countries mentioned in the article.
+
+    If a country is mentioned positively, the score should be close to 1; if negatively, close to -1.
+
+    actor_mentioned: a list of actors or organizations mentioned that play a role in the article.
+
+    article_type: choose exactly one type from the following list:
+    ["report", "analysis", "opinion", "interview", "investigation", "breaking_news", "feature"]
 }
-
-'''
-
-
 """
-strctured
-topics: the 2 main themes of the article, expressed in English (e.g., "military", "technology", "politics", "economy", "diplomacy", "environment", "health", "science", etc.).
-
-
-overall_sentiment: a global sentiment score between -1 (very negative) and 1 (very positive), with neutral sentiment close to 0.
-
-
-sentiment_by_country: a list of objects { "country": "<ISO alpha-2 code>", "sentiment": <float> } corresponding to the countries mentioned in the article.
-
-
-If a country is mentioned positively, the score should be close to 1; if negatively, close to -1.
-
-
-
-
-actor_mentioned: a list of actors or organizations mentioned that play a role in the article.
-
-
-article_type: choose exactly one type from the following list:
-["report", "analysis", "opinion", "interview", "investigation", "breaking_news", "feature"]
-
-"""
-
+articles = []
 # Charger les articles depuis le fichier JSON
 with open("../data/source_json_pour_llm.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
+    articles = json.load(f)
 
-articles = data.get("newsList", [])
-
+i=1
 # Liste pour stocker les articles enrichis
 enriched_articles = []
 for article in articles:
+    print(article)
+    
     try:
         messages = [
             {
                 "role": "user",
-                "content": "What is the best French meal? Return the name and the ingredients in short JSON object.",
+                "content": prompt,
+            },
+            {
+                "role": "user",
+                "content": str(article)
             }
         ]
         chat_response = client.chat.complete(
@@ -113,9 +54,14 @@ for article in articles:
             messages = messages,
             response_format = {
                 "type": "json_object",
+            },
+            prediction={
+
             }
         )
-        #enriched_articles.append(article)
+        enriched_articles.append(chat_response)
+        print(f"Article {i} fini")
+        i=i+1
     except Exception as e:
         print(f"⚠️ {article['id']} erreur : {e}")
 
@@ -124,4 +70,3 @@ with open("../data/articles_remplis_phase3_bis.json", "w", encoding="utf-8") as 
     json.dump({"newsList": enriched_articles}, f, ensure_ascii=False, indent=2)
 
 print("✅ Tous les articles enrichis ont été sauvegardés dans articles_enrichis.json")
-
